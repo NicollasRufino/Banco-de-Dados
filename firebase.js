ecdd43d1bf8455406e248f8088d4bc3ec93cbe22
NicollasRufino <nicollasrufinoo@gmail.com>
2022-06-29 12:50:58 -0300

    Semente do Banco de Dados

---
+++ firebase.js
@@ -0,0 +1,16 @@
+(function () {
+   
+      var firebaseConfig = {
+      apiKey: "AIzaSyD8N-mQsRzq0p-U7PyKDS-YeK673kLwhXU",
+      authDomain: "wall-e-457e1.firebaseapp.com",
+      projectId: "wall-e-457e1",
+      storageBucket: "wall-e-457e1.appspot.com",
+      messagingSenderId: "801182372746",
+      appId: "1:801182372746:web:3c99d511b379d9ec60ae73",
+      measurementId: "G-KC175GPV3T"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+}())
